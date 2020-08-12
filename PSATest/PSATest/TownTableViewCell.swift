a5d8b39015d0c0cb812220d478c0bf0702e41478
owee <casimir.luc@gmail.com>
2020-08-12 22:08:19 +0200

    Create Storyboard and UI interface. Add ListViewController, DetailViewController, SearchViewController, TownViewController

---
+++ PSATest/PSATest/TownTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TownTableViewCell.swift
+//  PSATest
+//
+//  Created by owee on 12/08/2020.
+//  Copyright © 2020 Devios. All rights reserved.
+//
+
+import UIKit
+
+class TownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
