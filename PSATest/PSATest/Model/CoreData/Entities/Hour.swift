376f540d5cdecc3339caf20838a0d4612856f894
owee <casimir.luc@gmail.com>
2020-08-15 18:39:00 +0200

    + add Day class in DataModel + add Hour class in DataModel

---
+++ PSATest/PSATest/Model/CoreData/Entities/Hour.swift
@@ -0,0 +1,14 @@
+//
+//  Hour.swift
+//  PSATest
+//
+//  Created by owee on 15/08/2020.
+//  Copyright © 2020 Devios. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Hour : NSManagedObject {
+    
+}
